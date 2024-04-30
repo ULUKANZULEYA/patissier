7a761f202018848adbeb630b57d95471e4a5660a
ULUKANZULEYA <166522465+ULUKANZULEYA@users.noreply.github.com>
2024-04-30 16:57:54 +0200

    patiserie

--- script.js
+++ script.js
@@ -8,22 +8,84 @@ fetch("patisseries.json")
   // Une fois que les données JSON sont extraites avec succès, jeles transmet à une fonction pour l'utiliser plus tard
   .then(data => {
     // Appelle la fonction addProducts() avec les données JSON en entrée
-    addProducts(data);
+    afficheLeHero(data)
+    addProducts(data.entreprise.produits);
+    afficherService(data.entreprise.services);
+    afficheAvis(data.entreprise.temoignages);
+    afficheAvantagesClients(data.entreprise.avantagesClients);
   });
 
+function afficheLeHero(donnee){
+document.querySelector("#hero").innerHTML+=`
+<div class="div2">
+    <h2>${donnee.entreprise.nomCommercial}</h2>
+    <h3>${donnee.entreprise.phraseAccroche}</h3>
+    <h3>${donnee.entreprise.texteAppelAction}</h3>
+</div>
+`
+}
+
+function afficheAvantagesClients(tableauAvantageClients){
+  //role: créer et quelle affiche les avantages clients dans mon html
+  //paramétre: tableau des avantages clients
+  tableauAvantageClients.forEach(element =>{
+  document.querySelector("#AC").innerHTML+=`
+  <div classe="div5">
+  <h3>${element}</h3>
+  
+  </div>
+  
+  `});
+}
+
 // Cette fonction prend les données JSON en entrée et effectue une action sur celles-ci
-function addProducts(data) {
+function addProducts(tableauDesProduits) {
+  // ceette fonction a besoin du tableau de produits
   // Affiche les données dans la console du navigateur
 
-  console.log(data);
+  //console.log(data);
+  tableauDesProduits.forEach(gateau => {
+    document.querySelector("#content").innerHTML +=`
+  <div class="carte">
+    <img src="${gateau.images}" alt="" class="imagg" >
+    <div>
+        <h3>${gateau.nom}</h3>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aspernatur rerum iusto ex dicta quaerat! Soluta quas quis consectetur sequi nesciunt iste est aut cum. </p>
+    </div>
+  </div>`
+  });
+}
+function afficherService(tableauDesServices){
+  tableauDesServices.forEach(srvc =>{
 
-data.forEach(element => {
-    let i="";
-     element.entreprise.forEach(entreprise=>{
-     i+=`<li> ${ingredients.produits} ${ingredients.services} ${ingredients.temoignages}</li>
-     })
-});
+document.querySelector("#pre").innerHTML +=`
+<div  >
+
+    <h3>${srvc.nom}</h3>
+    <p>${srvc.description}<p>
+
+</div>
+
+
+`});
+}
+function afficheAvis(tableauDesAvis){
+  tableauDesAvis.forEach(avs=>{
+
+  document.querySelector("#AVIS").innerHTML+=`
+  <div >
+      <h2>${avs.prenom}</h2>
+      <h3>${avs.typeExperience}</h3>
+      <h3>${avs.commentaire}</h3>
+      <h3>${avs.note}</h3>
+  </div>
+
+  `});
+}
+
+
+  
 
 
 
-    }
+
